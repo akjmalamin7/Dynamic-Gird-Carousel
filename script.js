849d832608d6ae2393c7a67127622433ddaaa570
akjmalamin7 <thpi205526@gmail.com>
2022-10-27 17:02:28 +0600

    slider translate

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let size = 100;
 let transition = 0.5;
 let speed = 3000;
 let gridDirection = "rightAlign"; // topAlign, bottomAlign, leftAlign, rightAlign
+let moveBothWays = "leftToRight" // leftToRight, rightTolLeft, topToBottom
 let isSlide = true;
+let parentDivElement;
 
 let isArrowController = true; // true or false
 let isDotController = true; // true or false
@@ -24,6 +26,8 @@ let carouselBannerImgArr = ["card.png", "card.png", "card.png"];
 let carouselSection = document.getElementById("gcl1_carousel_section");
 let carouselWrapper = document.getElementById("gcl1_carousel_wrapper");
 let carouselPushHere = document.getElementById("gcl1_carousel");
+let carouselInner = document.getElementById("gcl1_carousel_inner");
+let carouselSlide = carouselInner;
 
 let featureImageContainer = document.getElementById("gcl1_featured_images");
 let dotIndicatorPushHere = document.getElementById("gcl1_indicators");
@@ -59,12 +63,37 @@ if(carouselItemSize === 0){
 // slider create 
 (function () {
 	for( let i = 0; i<carouselItemSize; i++){
+
 		let carouselImgUrl = carouselImages[i];
-		console.log(carouselImgUrl);
-		
-	}
+
+		// create  and append carousel item
+		let carouselItem = elementMaker("div", ["gcl1_carousel_item",], `gcl1_carousel_item${i}`);
+		let carouselImgTag = elementMaker("img", [""], `gcl1_carousel_img${i}`);
+		carouselImgTag.src = carouselImgUrl;
+		carouselItem.appendChild(carouselImgTag);
+		carouselInner.appendChild(carouselItem);
+
+		// create append dot indicatorItem
+		let dotIndicatorItem = elementMaker("div", ["gcl1_indicator",]);
+		document.querySelector("gcl1_indicator")?.classList.add("gcl1_indicator_active");
+		dotIndicatorPushHere.appendChild(dotIndicatorItem);
+	};
+	parentDivElement = carouselInner;
+	let cloneFirstChild = parentDivElement.firstElementChild.cloneNode(true);
+	cloneFirstChild && (cloneFirstChild.id = "first_carousel_item");
+	// let cloneLastChild = parentDivElement.lastElementChild.cloneNode(true);
+	// cloneLastChild.id = "last_carousel_item";
+	parentDivElement && parentDivElement.appendChild(cloneFirstChild);
+	
+	// parentDivElement.insertAdjacentElement('afterbegin', cloneLastChild)
 })();
 
+if(moveBothWays === "leftToRight" || moveBothWays === "rightToLeft"){
+	carouselSlide && (carouselSlide.style.transform = "translateX(" + (-size*count) + "%)");
+
+}else if(moveBothWays === "topToBottom" || moveBothWays === "bottomToTop"){
+	carouselSlide && (carouselSlide.style.transform = "translateY(" + (-size*count) + "%)");
+}
 
 // carouselImage
 function carouselImage() {
@@ -87,7 +116,7 @@ function elementMaker(elemName, className, idName) {
   try {
     let element = document.createElement(elemName);
     className && (element.className = className.join(" "));
-    id && (element.id = idName);
+    idName && (element.id = idName);
     return element;
   } catch (err) {
     console.log(err);
